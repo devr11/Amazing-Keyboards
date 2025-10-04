3689419cf1a8bbb9bac9ec78e3e87acfc1b72d21
Dev <rastogi.db1@gmail.com>
2025-10-04 16:12:49 +0530

    Add new changes

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,7 +1,16 @@
 import * as THREE from "three";
-import React from "react";
+import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { ThreeEvent } from "@react-three/fiber";
+import gsap from "gsap";
+
+export const SOUND_MAP = {
+    red: ["/sounds/red-1.mp3", "/sounds/red-2.mp3", "/sounds/red-3.mp3" ],
+    blue: ["/sounds/blue-1.mp3", "/sounds/blue-2.mp3", "/sounds/blue-3.mp3" ],
+    black: ["/sounds/black-1.mp3", "/sounds/black-2.mp3", "/sounds/black-3.mp3" ],
+    brown: ["/sounds/brown-1.mp3", "/sounds/brown-2.mp3", "/sounds/brown-3.mp3" ],
+}
 
 // Type definitions
 type GLTFResult = GLTF & {
@@ -14,46 +23,141 @@ type GLTFResult = GLTF & {
   materials: Record<string, unknown>;
 };
 
-export function Switch() {
+type SwitchProps = React.ComponentProps<"group"> & {
+  color: "red" | "brown" | "black" | "blue";
+  hexColor: string;
+};
+
+export function Switch({ color, hexColor, ...restProps }: SwitchProps) {
   const { nodes } = useGLTF("/switch.gltf") as unknown as GLTFResult;
+  const switchGroupRef = useRef<THREE.Group>(null);
+  const stemRef = useRef<THREE.Mesh>(null);
+  const isPressedRef = useRef(false);
+  const audio = useRef<HTMLAudioElement>(null)
+  const audioTimeout = useRef<ReturnType<typeof setTimeout>>(null)
+  const allAudio = useRef(
+    SOUND_MAP[color].map((url)=>{
+        const audio = new Audio(url);
+        audio.volume = .6;
+        return audio;
+    })
+  )
+
+  const handlePointerDown = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+
+    if (!stemRef.current || !switchGroupRef.current || isPressedRef.current)
+      return;
+
+    isPressedRef.current = true;
+
+    const stem = stemRef.current;
+    const switchGroup = switchGroupRef.current;
+
+    gsap.to(switchGroup.rotation, {
+      x: Math.PI / 2 + 0.1,
+      duration: 0.05,
+      ease: "power2.out",
+    });
+
+    gsap.to(stem.position, {
+      z: 0.005,
+      duration: 0.08,
+      ease: "power2.out",
+    });
+
+    // Audio
+
+    audio.current = gsap.utils.random(allAudio.current)
+    audio.current.currentTime = 0;
+    audio.current.play()
+  };
+
+  const releaseSwitch = () => {
+
+    if (!stemRef.current || !switchGroupRef.current || !isPressedRef.current)
+      return;
+
+    isPressedRef.current = false;
+
+    const stem = stemRef.current;
+    const switchGroup = switchGroupRef.current;
+
+    gsap.to(switchGroup.rotation, {
+      x: Math.PI / 2,
+      duration: 0.6,
+      ease: "elastic.out(1, 0.3)",
+    });
+
+    gsap.to(stem.position, {
+      z: 0,
+      duration: 0.15,
+      ease: "elastic.out(1, 0.3)",
+    });
+  };
+
+  const handlePointerUp = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation()
+    releaseSwitch()
+  }
+
+  const handlePointerLeave = () => {
+    releaseSwitch()
+  }
 
   return (
-    <group scale={10} rotation={[Math.PI / 2, 0, 0]}>
-      {/* Switch housing */}
+    <group {...restProps}>
+      {/* Hit box */}
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Single_Switch_Mesh_1.geometry}
+        onPointerDown={handlePointerDown}
+        onPointerUp={handlePointerUp}
+        onPointerLeave={handlePointerLeave}
+        position={[0, 0.05, 0]}
+        onPointerOver={() => (document.body.style.cursor = "pointer")}
+        onPointerOut={() => (document.body.style.cursor = "default")}
       >
-        <meshStandardMaterial color="#999999" roughness={0.7} />
+        <boxGeometry args={[0.15, 0.15, 0.15]} />
+        <meshBasicMaterial transparent opacity={0} />
       </mesh>
 
-      {/* Gold contacts */}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Single_Switch_Mesh_2.geometry}
-      >
-        <meshStandardMaterial color="#ffd700" roughness={0.1} metalness={1} />
-      </mesh>
+      <group ref={switchGroupRef} scale={10} rotation={[Math.PI / 2, 0, 0]}>
+        {/* Switch housing */}
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Single_Switch_Mesh_1.geometry}
+        >
+          <meshStandardMaterial color="#999999" roughness={0.7} />
+        </mesh>
 
-      {/* Colored stem */}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Single_Switch_Mesh_3.geometry}
-      >
-        <meshStandardMaterial color={"#cccccc"} roughness={0.7} />
-      </mesh>
+        {/* Gold contacts */}
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Single_Switch_Mesh_2.geometry}
+        >
+          <meshStandardMaterial color="#ffd700" roughness={0.1} metalness={1} />
+        </mesh>
 
-      {/* Switch base */}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Single_Switch_Mesh_4.geometry}
-      >
-        <meshStandardMaterial color="#999999" roughness={0.7} />
-      </mesh>
+        {/* Colored stem */}
+        <mesh
+          ref={stemRef}
+          castShadow
+          receiveShadow
+          geometry={nodes.Single_Switch_Mesh_3.geometry}
+        >
+          <meshStandardMaterial color={hexColor} roughness={0.7} />
+        </mesh>
+
+        {/* Switch base */}
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Single_Switch_Mesh_4.geometry}
+        >
+          <meshStandardMaterial color="#999999" roughness={0.7} />
+        </mesh>
+      </group>
     </group>
   );
 }
--- src/slices/SwitchPlayground/index.tsx
+++ src/slices/SwitchPlayground/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FC } from "react";
 import { Content, isFilled } from "@prismicio/client";
 import {
@@ -10,6 +12,7 @@ import { FadeIn } from "@/components/FadeIn";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Switch } from "@/components/Switch";
+import { Stage } from "@react-three/drei";
 
 /**
  * Props for `SwitchPlayground`.
@@ -34,17 +37,18 @@ const SwitchPlayground: FC<SwitchPlaygroundProps> = ({ slice }) => {
         </h2>
 
         <div className="mb-6 max-w-4xl text-xl text-pretty">
-
-        <PrismicRichText field={slice.primary.description} />
+          <PrismicRichText field={slice.primary.description} />
         </div>
 
-        <FadeIn targetChildren className="grid grid-cols-1 gap-4 overflow-hidden sm:grid-cols-2">
-
-        {slice.primary.switches.map((item) =>
-          isFilled.contentRelationship(item.switch) ? (
-            <SharedCanvas key={item.switch.id} color={item.switch} />
-          ) : null,
-        )}
+        <FadeIn
+          targetChildren
+          className="grid grid-cols-1 gap-4 overflow-hidden sm:grid-cols-2"
+        >
+          {slice.primary.switches.map((item) =>
+            isFilled.contentRelationship(item.switch) ? (
+              <SharedCanvas key={item.switch.id} color={item.switch} />
+            ) : null,
+          )}
         </FadeIn>
       </FadeIn>
     </Bounded>
@@ -53,42 +57,50 @@ const SwitchPlayground: FC<SwitchPlaygroundProps> = ({ slice }) => {
 
 export default SwitchPlayground;
 
-
 type SharedCanvasProps = {
-  color: Content.SwitchPlaygroundSliceDefaultPrimarySwitchesItem["switch"]
-}
+  color: Content.SwitchPlaygroundSliceDefaultPrimarySwitchesItem["switch"];
+};
 
-const SharedCanvas = ({color}: SharedCanvasProps) => {
-  if(!isFilled.contentRelationship(color) || !color.data) return null;
-
+const SharedCanvas = ({ color }: SharedCanvasProps) => {
+  if (!isFilled.contentRelationship(color) || !color.data) return null;
 
   const colorName = color.uid as "red" | "brown" | "blue" | "black";
-  const {color: hexColor, name} = color.data
+  const { color: hexColor, name } = color.data;
 
   const bgColor = {
     blue: "bg-sky-950",
     red: "bg-red-950",
     black: "bg-gray-900",
     brown: "bg-amber-950",
-  }[colorName]
+  }[colorName];
 
   return (
     <div className="group relative min-h-96 overflow-hidden rounded-3xl select-none">
       {/* Text button */}
       {/* Canvas */}
-      <Canvas>
-        <Switch />
+      <Canvas camera={{position:[1.5,2,0], fov:7}}>
+        <Stage adjustCamera intensity={.5} shadows={"contact"} environment={"city"}>
+          <Switch rotation={[0, Math.PI/4, 0]} color={colorName} hexColor={hexColor || ""} />
+        </Stage>
       </Canvas>
 
-      <div className={clsx(
-        "font-black-slanted absolute inset-0 -z-10 grid place-items-center text-8xl uppercase",
-        bgColor,
-      )}>
-
+      <div
+        className={clsx(
+          "font-black-slanted absolute inset-0 -z-10 grid place-items-center text-8xl uppercase",
+          bgColor,
+        )}
+      >
         <svg className="pointer-events-none h-auto w-full" viewBox="0 0 75 100">
-          <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize={18} className="font-black-slanted fill-white/30 uppercase mix-blend-overlay group-hover:fill-white/100 motion-safe:transition-all motion-safe:duration-700">
-            {Array.from({length:8}, (_, i)=>(
-              <tspan key={i} x={`${(i+1) * 10}%`} dy={i == 0 ? -40 : 14}>
+          <text
+            x="50%"
+            y="50%"
+            dominantBaseline="middle"
+            textAnchor="middle"
+            fontSize={18}
+            className="font-black-slanted fill-white/30 uppercase mix-blend-overlay group-hover:fill-white/100 motion-safe:transition-all motion-safe:duration-700"
+          >
+            {Array.from({ length: 8 }, (_, i) => (
+              <tspan key={i} x={`${(i + 1) * 10}%`} dy={i == 0 ? -40 : 14}>
                 {colorName}
                 {colorName}
                 {colorName}
@@ -98,5 +110,5 @@ const SharedCanvas = ({color}: SharedCanvasProps) => {
         </svg>
       </div>
     </div>
-  )
-}
+  );
+};
