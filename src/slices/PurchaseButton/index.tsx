5f70725f759aa5e2c4b3fbe0d0267b143d20761f
Dev <rastogi.db1@gmail.com>
2025-10-06 10:22:44 +0530

    Add new changes

--- src/slices/PurchaseButton/index.tsx
+++ src/slices/PurchaseButton/index.tsx
@@ -8,6 +8,7 @@ import {
 import { Bounded } from "@/components/Bounded";
 import { FadeIn } from "@/components/FadeIn";
 import clsx from "clsx";
+import { LuChevronRight } from "react-icons/lu";
 
 /**
  * Props for `PurchaseButton`.
@@ -38,13 +39,29 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
 
         <button
           className={clsx(
-            "group relative w-full overflow-hidden rounded-full border-8 border-gray-900 bg-linear-to-r/oklch from-sky-300 to-sky-600 px-6 py-6 ease-out focus:ring-[24px] focus:ring-sky-500/50 focus:outline-none",
+            "group relative w-full overflow-hidden rounded-full border-8 border-gray-900 bg-linear-to-r/oklch from-sky-300 to-sky-600 px-6 py-6 ease-out focus:ring-[24px] focus:ring-sky-500/50 focus:outline-none motion-safe:transition-all motion-safe:duration-300 md:border-[12px] md:px-20 md:py-16",
+            "hover:scale-105 hover:shadow-2xl hover:shadow-sky-500/40",
+            "cursor-pointer active:scale-95",
           )}
         >
-          {slice.primary.button_text}
+          <div className="absolute inset-0 -translate-x-full bg-linear-to-r from-transparent via-white/40 to-transparent ease-out group-hover:translate-x-full motion-safe:transition-transform motion-safe:duration-1000" />
+
+          <div className="relative z-10 flex items-center justify-center gap-6 md:gap-8">
+            <span className="font-black-slanted text-4xl tracking-wide text-gray-900 uppercase group-hover:-translate-y-1 motion-safe:transition-transform motion-safe:duration-300 md:text-7xl lg:text-9xl">
+              {slice.primary.button_text}
+            </span>
+
+            <div className="hidden group-hover:translate-x-2 group-hover:scale-125 motion-safe:transition-all motion-safe:duration-300 md:block">
+              <LuChevronRight className="size-12 text-gray-900 md:size-16" />
+            </div>
+          </div>
         </button>
 
+        <div className="m-12 space-y-3 text-base">
+
+
         <PrismicRichText field={slice.primary.body} />
+        </div>
       </FadeIn>
     </Bounded>
   );
